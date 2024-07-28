25b8439e721b25bc38e6cc2c1feaa2042d3892da
VSanjith-08 <vsanjith.ind@gmail.com>
2024-07-28 20:39:20 +0530

    Modified login.py file

--- login.py
+++ login.py
@@ -9,23 +9,19 @@ mydb = mysql.connector.connect(
     passwd = "sanjith",
     database = "Bank_Management"
 )
-
-
-
+mycursor = mydb.cursor()
 
 def signup():
     state = input("ENTER YOUR STATE : ")
     fname = input("ENTER YOUR FIRSTNAME  : ")
     lname = input("ENTER YOUR LASTNAME : ")
-    dob = input("ENTER YOUR DATE OF BIRTH : ")
+    dob = input("ENTER YOUR DATE OF BIRTH (yyyy/mm/dd) : ")
     mobile = input("ENTER YOUR MOBILE NUMBER : ")
-    user_info=(state,fname,name,dob,mobile)
-
-    mycursor = mydb.cursor()
-
-    formula1 = "import into login_details values(%s,%s,%s,%s,%s)"
-    mycursor.execute(user_info)
-    mydb.commit()
+    global user_info
+    user_info=(fname,lname,state,mobile,dob)
     
-
 signup()
+
+formula1 = "insert into login_details (firstname,lastname,state,mobileno,dob) values (%s,%s,%s,%s,%s)"
+mycursor.execute(formula1,("V","Sanjith","tn",'9446324247',"2007-08-31"))
+mydb.commit()
