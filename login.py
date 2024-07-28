772652cba2ef5bb8e363e7c13edb49869da68e3a
VSanjith-08 <vsanjith.ind@gmail.com>
2024-07-28 22:42:39 +0530

    Modified login.py file

--- login.py
+++ login.py
@@ -12,16 +12,54 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 def signup():
-    state = input("ENTER YOUR STATE : ")
-    fname = input("ENTER YOUR FIRSTNAME  : ")
-    lname = input("ENTER YOUR LASTNAME : ")
-    dob = input("ENTER YOUR DATE OF BIRTH (yyyy/mm/dd) : ")
-    mobile = input("ENTER YOUR MOBILE NUMBER : ")
+    fname = input("FIRSTNAME : ").upper()
+    lname = input("LASTNAME: ").upper()
+    state = input("STATE CODE (Eg: tn,ka,kl,etc): ").lower()
+    gender = input("GENDER: ").upper()
+    dob = input("DATE OF BIRTH (yyyy-mm-dd) : ")
+    def mob_le():
+        global mobile
+        mobile = input("MOBILE NUMBER : ")
+        a=0
+        for i in mobile:
+            if i.isdigit()==False:
+                print("\nERROR: RE-ENTER YOU'RE MOBILE NUMBER\n")
+                mob_le()
+            a+=1
+        if a!=10:
+            print("\nERROR: RE-ENTER YOU'RE MOBILE NUMBER\n")
+            mob_le()
+    mob_le()
+    def passwd():
+        n_passwd = input("PASSWORD: ")
+        caps,small,digit,space,spc = False,False,False,False,False
+        for i in n_passwd:
+            if i.isupper():
+                caps = True
+            if i.islower():
+                small = True
+            if i.isspace():
+                space = True
+            if i.isdigit():
+                digit = True
+            if [i.isdigit(),i.isalpha(),i.isspace()] == [False,False,False]:
+                spc = True
+        if [caps,small,digit,space,spc] != [True,True,True,False,True]:
+            print("\nWRONG INPUT!\nYOUR PASSWORD SHOULD HAVE ATLEAST ONE CAPITAL,SMALL,DIGIT AND NO SPACE.\n")
+            passwd()
+        global c_passwd
+        c_passwd = input("REPEAT PASSWORD: ")
+        if n_passwd == c_passwd:
+            pass
+        else:
+            print("\nERROR: PASSWORDS DOES NOT MATCH\n")
+            passwd()
+    passwd()
     global user_info
-    user_info=(fname,lname,state,mobile,dob)
+    user_info=(fname,lname,state,mobile,dob,c_passwd,gender)
     
 signup()
 
-formula1 = "insert into login_details (firstname,lastname,state,mobileno,dob) values (%s,%s,%s,%s,%s)"
-mycursor.execute(formula1,("V","Sanjith","tn",'9446324247',"2007-08-31"))
+formula1 = "insert into login_details (firstname,lastname,state,mobileno,dob,passwd,gender) values (%s,%s,%s,%s,%s,%s,%s)"
+mycursor.execute(formula1,user_info)
 mydb.commit()
