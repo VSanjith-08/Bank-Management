12dc57b50430acf3f314a30ac48210b28dac96cb
SANJAY-175 <sanjaythebeast175@gmail.com>
2024-07-28 17:41:37 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,7 +1,11 @@
 # this is the login module
 
 import pandas as pd
+
+
+def
 input("Enter your state: ")
 input("Enter your firstname: ")
 input("Enter your lastname: ")
-input("Enter your 
+input("Enter your Date of birth: ")
+input("Enter your mobile number: ")
