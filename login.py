466b023b2e278794cc48675a91a6c2a16d91479a
SANJAY-175 <sanjaythebeast175@gmail.com>
2024-07-28 18:29:06 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,11 +1,15 @@
 # this is the login module
 
 import pandas as pd
+impoet mysql.connector
+
+mydb = mysql.connector.connect
 
 
-def log():
-  input("Enter your state: ")
-  input("Enter your firstname: ")
-  input("Enter your lastname: ")
-  input("Enter your Date of birth: ")
-  input("Enter your mobile number: ")
+def signup():
+  state = input("ENTER YOUR STATE : ")
+  fname = input("ENTER YOUR FIRSTNAME  : ")
+  lname = input("ENTER YOUR LASTNAME : ")
+  dob = input("ENTER YOUR DATE OF BIRTH : ")
+  mobile = input("ENTER YOUR MOBILE NUMBER : ")
+  user_info=(state,fname,name,dob,mobile)
